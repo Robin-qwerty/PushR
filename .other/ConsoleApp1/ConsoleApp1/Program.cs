7f8f85f48b738b67dfcc3bcf8ae7b50ef9ce0e7a
Robin-qwerty <robin@humilis.net>
2023-05-08 09:06:23 +0200

    remove llvm language

---
+++ .other/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            bool b = false;
+
+            var tern = x > 10 ? b = true : b = false;
+
+
+            if ( x> 10 ) {
+                b = true;
+            }
+            else
+            {
+                b = false;
+            }
+
+        }
+    }
+}
